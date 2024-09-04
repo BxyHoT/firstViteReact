2d608f8d5e021e076e69c1b88d3245cbdc937d95
BxyHoT <bhynot@gmail.com>
2024-09-04 17:35:01 +0300

    отображение колличества тасок

--- src/modules/Footer/Footer.tsx
+++ src/modules/Footer/Footer.tsx
@@ -1,10 +1,10 @@
 import "../../App.css";
 import { TaskFilter } from "../TaskFilter/TaskFilter";
 
-export const Footer = () => {
+export const Footer = ({ taskLength }: { taskLength: number }) => {
   return (
     <footer className="footer">
-      <span className="todo-count">1 items left</span>
+      <span className="todo-count">{taskLength} items left</span>
       <TaskFilter />
       <button className="clear-completed">Clear completed</button>
     </footer>
