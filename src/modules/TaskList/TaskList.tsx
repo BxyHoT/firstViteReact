4eed137ebeff709758a739ce84b9f14089ec57ab
BxyHoT <bhynot@gmail.com>
2024-09-05 21:31:20 +0300

    переписал тасклист на классы

--- src/modules/TaskList/TaskList.tsx
+++ src/modules/TaskList/TaskList.tsx
@@ -1,31 +1,44 @@
 import "../../App.css";
 import { Task } from "../Task/Task";
 import { ITask, HandleFunctions } from "../../App";
+import { Component } from "react";
 
-export const TaskList = ({
-  tasksArray,
-  handleChangeDone,
-  handleTaskDelete,
-}: {
+interface ITasksListProps {
   tasksArray: ITask[];
   handleChangeDone: HandleFunctions;
   handleTaskDelete: HandleFunctions;
-}) => {
-  const tasks = tasksArray.map((task) => {
-    const { id, content, isDone } = task;
+}
 
-    return (
-      <li className={isDone ? "completed" : ""} key={id}>
-        <Task
-          id={id}
-          content={content}
-          isDone={isDone}
-          handleChangeDone={handleChangeDone}
-          handleTaskDelete={handleTaskDelete}
-        />
-      </li>
-    );
-  });
+export class TaskList extends Component<ITasksListProps> {
+  render() {
+    const { tasksArray, handleChangeDone, handleTaskDelete } = this.props;
 
-  return <ul className="todo-list">{tasks}</ul>;
-};
+    const tasks = tasksArray.map((task) => {
+      const { id, content, isDone } = task;
+
+      return (
+        <li className={isDone ? "completed" : ""} key={id}>
+          <Task
+            id={id}
+            content={content}
+            isDone={isDone}
+            handleChangeDone={handleChangeDone}
+            handleTaskDelete={handleTaskDelete}
+          />
+        </li>
+      );
+    });
+
+    return <ul className="todo-list">{tasks}</ul>;
+  }
+}
+
+// export const TaskList = ({
+//   tasksArray,
+//   handleChangeDone,
+//   handleTaskDelete,
+// }: {
+//   tasksArray: ITask[];
+//   handleChangeDone: HandleFunctions;
+//   handleTaskDelete: HandleFunctions;
+// }) => {};
